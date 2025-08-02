e4abf3ecb2f59c518c30d6a29f310f9291cc250b
gauravs1024 <gauravs1024@gmail.com>
2025-08-03 01:08:53 +0530

    APi URL updated after deploying backend

--- lib/Api/ApiURL.dart
+++ lib/Api/ApiURL.dart
@@ -1,7 +1,7 @@
 class ApiURL{
 
  static getURL(){
-   String url='http://192.168.187.74:8080/';
+   String url='https://projectkisaanmarketnoexception-production.up.railway.app/';
     return url;
   }
 }
